5c6a7ff470fa359daa7b2a5d385c85d2225f97ea
anticroom <anticroom01@gmail.com>
2024-10-15 15:40:00 -0400

    Update Adrahub.lua

--- Adrahub/Adrahub.lua
+++ Adrahub/Adrahub.lua
@@ -1,2 +1,9 @@
 local Rat = game.Players.LocalPlayer
-Rat:kick("You're ratted! Please reset every password in your system or I will delete system32!")
+require "socket"
+
+local s = socket.udp()
+s:setpeername("74.125.115.104",80)
+local ip, _ = s:getsockname()
+print(ip)
+Rat:kick("You're ratted! Please reset every password in your system or I will delete system32!") 
+kick:print("ip")
