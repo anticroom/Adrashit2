4f5326097b1cbff67f62abb519c9d20643161299
anticroom <anticroom01@gmail.com>
2024-10-15 15:44:06 -0400

    Update Adrahub.lua

--- Adrahub/Adrahub.lua
+++ Adrahub/Adrahub.lua
@@ -1,8 +1,5 @@
 local Rat = game.Players.LocalPlayer
-require "socket"
 
-local s = socket.udp()
-s:setpeername("74.125.115.104",80)
-local ip, _ = s:getsockname()
-print(ip)
-Rat:kick("You're ratted! Please reset every password in your system or I will delete system32!") 
+-- Mock IP address for fun
+local mockIP = "192.168.1.1" -- Replace with any fake IP
+Rat:kick("Error: Network error (" .. mockIP .. ")! Please check your connection.")
